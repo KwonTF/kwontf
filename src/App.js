934d212a41ec257ef3093271737a1dddf7db2c6e
KwonTF <ktf1008@ssu.ac.kr>
2017-11-17 14:52:17 +0900

    이미지 초안

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import './app.css';
-
 class App extends React.Component {
   render() {
     return (
       <div className="App">
         <div className="Title">
-          <h1>Kwon Practical Series</h1>
+          <TextGlitcher inputText = "Kwon Practical Series" />
           <h3>Still Image / BGA</h3>
         </div>
         <div className="Image">
           <Image/>
+        </div>
+        <div className="VGA">
+          <h1>Video</h1>
         </div>
       </div>
     );
@@ -23,30 +25,84 @@ class Image extends React.Component{
       ImageArray: Array.from({length: 18}, (v, k) => k+1)
     };
   }
-  render(){
+    render(){
     return(
       <div>
         <h4>Image</h4>
+        <div className="imageFullContainer">
         {
           this.value.ImageArray.map((i) => {
             return(
-              <ImageClass num = {i} key = {i}/>
+              <ImageClass key = {i} num = {i}/>
             );
           })
         }
+        </div>
       </div>
     );
   }
 }
-
+//
 class ImageClass extends React.Component{
  render(){
+   let data = (require('./ImageData.json')).ImageMeta;
+   let rowNum = data[(this.props.num)-1].text.split('</br>').length;
    return(
-   <div>
-     <img src = {"./img/"+ this.props.num + ".png"} alt = {this.props.num}/>
-     <p>{this.props.num}</p>
+   <div className = "imageContainer">
+     <img src = {"./img/"+ this.props.num + ".png"} alt = {this.props.num} className = {"image"+this.props.num}/>
+       <p className="ImageText">{
+         data[(this.props.num)-1].text.split('</br>').map((line,index)=>{
+          if(index+1 === rowNum){
+            <span key={this.props.num+"of"+index}>{line}</span>
+          }
+       return(
+         <span key={this.props.num+"of"+index}>{line}<br/></span>
+       )
+     })}</p>
    </div>);
  }
+}
+class TextGlitcher extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = { OriginText: this.props.inputText, Text: Array.from({length: this.props.inputText.length}, () => ' ').join(''), elapsed:0,  start: Date.now()};
+  }
+  componentDidMount(){
+    this.timer = setInterval(()=>this.tick(), 80);
+  }
+  componentWillUnmount(){
+    clearInterval(this.timer);
+  }
+  tick(){
+    this.setState({elapsed: new Date() - this.state.start});
+    let charSelector = Math.floor(Math.random()*this.state.OriginText.length);
+    let newText = this.state.Text.substr(0, charSelector) + this.state.OriginText.charAt(charSelector) + this.state.Text.substr(charSelector+1);
+    this.setState({Text: newText});
+    if(this.state.Text === this.state.OriginText){
+      clearInterval(this.timer);
+      this.timer = setInterval(()=>this.tick2(Math.random()*this.state.Text.length), 2500);
+    }
+  }
+  tick2(randomNumber){
+    let tempchar = Math.random().toString(36).substr(2,1);
+    if(Math.random() >= 0.5){
+      tempchar = tempchar.toUpperCase();
+    }
+    let newText = this.state.Text.substr(0, randomNumber) + tempchar + this.state.Text.substr(randomNumber+1);
+    this.setState({Text: newText});
+    this.timer = setTimeout(()=>this.recover(randomNumber), 200);
+  }
+  recover(index){
+    let newText = this.state.Text.substr(0, index) + this.state.OriginText.charAt(index) + this.state.Text.substr(index+1);
+    this.setState({Text: newText});
+  }
+  render(){
+    // This will give a number with one digit after the decimal dot (xx.x): 
+
+    // Although we return an entire <p> element, react will smartly update
+    // only the changed parts, which contain the seconds variable.
+    return <p className = "text">{this.state.Text}</p>;
+  }
 }
 export default App;
 /*

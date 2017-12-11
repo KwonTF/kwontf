82508daf98bc354d77588087f5f8a8475463cf61
KwonTF <ktf1008@ssu.ac.kr>
2017-12-11 14:12:25 +0900

    v0.3 start
    Da!

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ class App extends Component {
           <Video/>
         </div>
         <div>
-          <h4 className="Credit">Kwontf.Net Version 0.2 <br/>Telegram: @ktf1008<br/> Kakao, Email: ktf1008@naver.com </h4>
+          <h4 className="Credit">Kwontf.Net Version 0.3 <br/>Telegram: @ktf1008<br/> Kakao, Email: ktf1008@naver.com </h4>
           </div>
       </div>
       </div>

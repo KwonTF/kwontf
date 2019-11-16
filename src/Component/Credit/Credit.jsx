e2293f393831f068a353c6cc356524ae3d2339e9
KwonTF <ktf1008@naver.com>
2019-11-17 00:27:56 +0900

    버젼 업그레이드같은거 처음해봄
    이거 잘못해서 꼬일까봐 겁남

--- src/Component/Credit/Credit.jsx
+++ src/Component/Credit/Credit.jsx
@@ -5,7 +5,7 @@ import './Credit.css';
 function Credit() {
   return (
     <p className="Credit">
-      Kwontf.Net v1.0<br />
+      Kwontf.Net v1.0.1<br />
       Telegram: @ktf1008<br />
       Kakao, Email: ktf1008@naver.com
     </p>
